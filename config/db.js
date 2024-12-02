40a9d3ce7780819c797c77297a501230d7c25473
EmerickGrimm <62389455+EmerickGrimm@users.noreply.github.com>
2024-12-02 13:16:07 +0200

    fix

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 
-dotenv.config();
+dotenv.config({ path: require('path').resolve(__dirname, '.env') });
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,         
